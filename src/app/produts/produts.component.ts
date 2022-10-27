f45c011c090a397208677791bf7db52d87b24b1c
Octávio Manuel Gonçalves Fernandes <octavio_mgf@hotmail.com>
2022-10-27 15:51:35 +0100

    filtros doooone!! you gooo boy

--- src/app/produts/produts.component.ts
+++ src/app/produts/produts.component.ts
@@ -15,10 +15,13 @@ export class ProdutsComponent implements OnInit {
   totalProducts!: number;
   recPage: number = 6;
   productsList: Product[] = [];
-  seeMoreButton:boolean = true;
+  seeMoreButton: boolean = true;
+
+  url: string[] = [];
+  addUrl: string = "";
 
 
-  constructor(private servProd: ProductsserviceService, private router:Router) { }
+  constructor(private servProd: ProductsserviceService, private router: Router) { }
 
   ngOnInit(): void {
     this.loadColorsAndTipes();
@@ -48,36 +51,98 @@ export class ProdutsComponent implements OnInit {
     })
   }
 
-  filter(key: string, value: string) {
-    console.log(key + ' ' + value);
-  }
-
   getPaginateProducts() {
     this.servProd.searchProducts(this.recPage).subscribe(response => {
 
-      this.productsList=response;
+      this.productsList = response;
       console.log(this.productsList);
 
     });
   }
 
-  seeMoreProducts(){
+  seeMoreProducts() {
 
     if ((this.recPage + 6) >= this.totalProducts) {
 
       this.recPage = this.totalProducts;
       this.seeMoreButton = false;
 
-    }else{
-      this.recPage = this.recPage + 6 ;
+    } else {
+      this.recPage = this.recPage + 6;
     }
     this.getPaginateProducts();
-    
+
     console.log(this.recPage);
 
+    console.log(this.servProd.urlAPIseacrh);
+
+    this.servProd.urlAPIseacrh += (`limit=${this.recPage}`);
+
+    console.log(this.servProd.urlAPIseacrh);
+
   }
-  showProductInfo(id: number){
-    this.router.navigateByUrl(`/infoproducts/${id}`)
+
+  filterProducts(key: string, value: string) {
+
+    let field: string = "";
+
+    this.addUrl="";
+
+    if (key === "Tipo") {
+
+      field = "&tipo_de_produto=" + value;
+      console.log(field);
+
+      if (!this.url.includes(field)) {
+        this.url.push(field);
+
+      } else {
+        this.url.splice(this.url.indexOf(field), 1);
+      }
+
+
+    } else {
+      field = "&cor=" + value;
+      console.log(field);
+
+      if (!this.url.includes(field)) {
+        this.url.push(field);
+
+      } else {
+        this.url.splice(this.url.indexOf(field), 1);
+      }
+
+    }
+
+    // field = key + value;
+
+    console.log(this.url);
+
+    console.log(this.addUrl);
+
+    for (let i = 0; i < this.url.length; i++) {
+      this.addUrl += this.url[i];      
+    }
+    
+    console.log("AddURL:"+this.addUrl);
+
+    this.servProd.filterProducts(this.recPage,this.addUrl).subscribe(response=>{
+      this.productsList = response;
+      this.totalProducts = response.length;
+    })
+
+
+    //http://localhost:3000/products?limit=6&tipo_de_produto=Calças&cor=Azul&cor=Laranja&tipo_de_produto=Casaco
+
+
+  }
+
+
+
+
+
+  showProductInfo(id: number) {
+    this.router.navigateByUrl(`/infoproducts/${id}`);
   }
 
 }
--- src/app/shared/productsservice.service.ts
+++ src/app/shared/productsservice.service.ts
@@ -9,6 +9,8 @@ export class ProductsserviceService {
 
   private urlAPI = "http://localhost:3000/products";
 
+  public urlAPIseacrh : string = "http://localhost:3000/products";
+
   constructor(private http : HttpClient) { }
 
   getProducts(){
@@ -25,8 +27,18 @@ export class ProductsserviceService {
 
   }
 
+  filterProducts(recPage: number, filter: string){
+
+    console.log(`${this.urlAPI}?_limit=${recPage}${filter}`);
+
+    return this.http.get<Product[]>(`${this.urlAPI}?_limit=${recPage}${filter}`)
+
+  }
+
   //http://localhost:3000/products?tipo_de_produto=Calças&cor=Azul&_limit=100
-  
+
+  //http://localhost:3000/products?limit=6&tipo_de_produto=Calças&cor=Azul&cor=Laranja&tipo_de_produto=Casaco
+
 
   // pesquisaPhotos(pesquisa : string, initial : number, recPage: number) {
   //   return this.http.get<Photo[]>(`${this.urlAPI}?title_like=${pesquisa}&_start=${initial}&_limit=${recPage}`, { observe : 'response' })
